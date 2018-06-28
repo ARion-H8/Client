5585f83a7bb221a05a1e4b68b1a52c3cb4695862
Renal Apriansyah <renal@gmail.com>
2018-06-28 15:00:39 +0700

    add all page with navigation

---
+++ pages/Catalogue.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+export default class Catalogue extends Component {
+  render() {
+    <View>
+			<Text>
+				Catalogue
+			</Text>
+		</View>
+  }
+}
+
---
+++ pages/Detail.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+export default class Detail extends Component {
+  render() {
+    <View>
+			<Text>
+				Detail
+			</Text>
+		</View>
+  }
+}
+
