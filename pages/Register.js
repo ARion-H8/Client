65a0f488294b74cf578da5bf559302984d387971
Natascha Lamsu <tazcha15@gmail.com>
2018-06-29 09:52:39 +0700

    regis and login pages native base

--- pages/Register.js
+++ pages/Register.js
@@ -1,71 +1,67 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  TextInput
-} from 'react-native';
+import { Container, Content, Form, Item, Input, Label, Button, Text, H1 } from 'native-base';
 
 class Register extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.title}>
-            Register
-          </Text>
-          <Text style={styles.subtitle}>Username:</Text>
-          <TextInput
-          name='username'
-          placeholder='Username...'
-          style={styles.textInput}
-          />
-          <Text style={styles.subtitle}>Email:</Text>
-          <TextInput
-          name='email'
-          placeholder='Email...'
-          style={styles.textInput}
-          />
-          <Text style={styles.subtitle}>Password:</Text>
-          <TextInput
-          name='password'
-          placeholder='Password...'
-          style={styles.textInput}
-          />
-          <Button
-          title='Sign Up'
-          onPress={() => this.props.navigation.navigate('Catalogue')}
-          />
-        </View>
-      </View>
+      <Container>
+        <Content style={{ marginVertical: 220, marginHorizontal:10, backgroundColor: '#f1ded3', borderRadius: 10, paddingRight: 20, paddingTop: 15 }}>
+          <Form style={{ alignItems: 'center', paddingVertical: 0 }}>
+            <H1 style={{ alignSelf: 'center', fontWeight: 'bold' }}>Register</H1>
+            <Item fixedLabel>
+              <Label>Username</Label>
+              <Input 
+              name='username'
+              />
+            </Item>
+            <Item fixedLabel>
+              <Label>Email</Label>
+              <Input 
+              name='email'
+              />
+            </Item>
+            <Item fixedLabel>
+              <Label>Password</Label>
+              <Input
+              name='password'
+              secureTextEntry={true}
+              />
+            </Item>
+          </Form>
+          <Button 
+          success
+          style={{ marginTop: 20, alignSelf: 'center' }}>
+            <Text> Sign Up </Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 3,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 1
-  },
-  textInput: {
-    width: 300,
-    padding: 8
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    margin: 5,
-    alignSelf: 'center'
-  },
-  subtitle: {
-    fontWeight: 'bold',
-    fontSize: 15,
-    margin: 2
-  }
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 3,
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     margin: 1
+//   },
+//   textInput: {
+//     width: 300,
+//     padding: 8
+//   },
+//   title: {
+//     fontWeight: 'bold',
+//     fontSize: 25,
+//     margin: 5,
+//     alignSelf: 'center'
+//   },
+//   subtitle: {
+//     fontWeight: 'bold',
+//     fontSize: 15,
+//     margin: 2
+//   }
+// })
 
 export default Register;
