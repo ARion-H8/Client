5aa50f33f68974f2472d255960e26faa8c2e8c78
Natascha Lamsu <tazcha15@gmail.com>
2018-06-30 13:24:22 +0700

    detail and cart pages

--- pages/Detail.js
+++ pages/Detail.js
@@ -11,12 +11,12 @@ export default class Detail extends Component {
   render() {
     return (
       <Container style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f1ded3' }}>
-        <Content style={{ padding: 10, marginTop: 30, width: 400, marginBottom: 170, backgroundColor: '#cd9774', borderRadius: 10 }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Nama Barang</Text>
-          <Image source={require('./spiker.jpeg')} style={{ borderRadius: 10, margin: 10, alignSelf: 'center', width: 380, height: 380 }}/>
-          <Text>Harga Barang</Text>
+        <Content style={{ padding: 10, marginTop: 30, width: 380, marginBottom: 170, borderRadius: 10 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 20, borderBottomWidth: 1, padding: 5 }}>Nama Barang</Text>
+          <Image source={require('./spiker.jpeg')} style={{ borderWidth: 1, borderColor: 'grey', borderRadius: 10, margin: 10, alignSelf: 'center', width: 360, height: 360 }}/>
+          <Text style={{ borderBottomWidth: 1, padding: 5 }}>Harga Barang</Text>
           <Container style={{ flexDirection: 'row', marginTop: 10, height: 50 }}>
-              <Button success style={{ marginRight: 190  }}><Text>Cek Barang</Text></Button>
+              <Button success style={{ marginRight: 150  }}><Text>Cek Barang</Text></Button>
             <Right>
               <Button success><Text>Beli</Text></Button>
             </Right>
