eb772b3ac915018c106e7cd1fc09d52ae8d9bf58
Natascha Lamsu <tazcha15@gmail.com>
2018-07-02 17:52:03 +0700

    components and pages for cart

--- pages/Cart.js
+++ pages/Cart.js
@@ -21,29 +21,55 @@ import {
   Footer,
   FooterTab 
 } from 'native-base';
+import ListCart from '../components/ListCart.js'
+import Total from '../components/Total.js'
+import product from '../Graphql'
+import { graphql, compose } from 'react-apollo'
 
-export default class Cart extends Component {
+class Cart extends Component {
   constructor () {
     super ()
     this.state = {
-      number: 0,
+      totalPrice: 0,
       data: [
         {
           nama: 'item1',
-          harga: '10000',
-          jumlah: 0
+          harga: 10000
         }, {
           nama: 'item2',
-          harga: '20000',
-          jumlah: 0
+          harga: 20000
         }, {
           nama: 'item3',
-          harga: '30000',
-          jumlah: 0
+          harga: 30000
         }
       ],
-      totalPrice: 0
+      quantity: null
     }
+  }
+
+  componentDidMount() {
+    const { user } = this.props.data
+    console.log("user >>", this.props)
+    // let total = 0
+    // user.cart.map((item) => {
+    //   total += item.product.price
+    // })
+    // this.setState({
+    //   totalPrice: total
+    // })
+    // this.forTotalPrice()
+  }
+
+  forTotalPrice = (total) => {
+    console.log('total =====>', total)
+    // this.state.data.map((item) => {
+    //   this.state.totalPrice = this.state.totalPrice + item.harga
+    // })
+    let totalItem = 0
+    totalItem += total
+    this.setState({
+      totalPrice: totalItem
+    })
   }
 
   rupiah = (num) => {
@@ -52,93 +78,36 @@ export default class Cart extends Component {
     return  `Rp. ${result}`
   }
 
-  forTotalPrice = (price) => {
-    this.setState({
-      totalPrice: this.state.totalPrice + price
-    })
-  }
-
-  handlePlus = (jumlah) => {
-    this.setState({
-      number: this.state.number + 1
-    })
-    console.log('+++++>', this.state.number)
-  }
-
-  handleMinus = (jumlah) => {
-    if (this.state.number > 0) {
-      this.setState({
-        number: this.state.number - 1
-      })
-    }
-    console.log('----->', this.state.number)
-  }
-
   render() {
-    return (
-      <Container style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ebeced' }}>
-        <Content style={{ padding: 10, marginTop: 25, marginBottom: 10, width: 400 }}>
-        <FlatList
-        data={this.state.data}
-        keyExtractor={(item) => item.nama}
-        renderItem={({item}) => 
-          <Card>
-            <CardItem cardBody>
-              <Left>
-                <Image source={require('./spiker.jpeg')} style={{height: 200, width: 200 }}/>
-              </Left>
-              <Left>
-                <Body>
-                  <Text style={{ alignSelf: 'flex-start', fontWeight: 'bold', color: '#3e3e3b' }}>{item.nama}</Text>
-                  <Text style={{ alignSelf: 'flex-start', color: '#3e3e3b' }}>{this.rupiah(item.harga)}</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Left style={{ alignItems: 'flex-start' }}>
-                <Button
-                rounded
-                success
-                onPress={() => item.jumlah--}
-                >
-                  <Text style={{ fontWeight: 'bold' }}> - </Text>
-                </Button>
-                <Button bordered rounded light>
-                  <Text style={{ color: 'black' }}> {item.jumlah} </Text>
-                </Button>
-                <Button
-                rounded
-                success
-                onPress={() => item.jumlah++}
-                >
-                  <Text style={{ fontWeight: 'bold' }}> + </Text>
-                </Button>
-              </Left>
-              <Right>
-                <Button 
-                style={{ borderRadius: 5 }} 
-                danger>
-                  <Text>Delete</Text>
-                </Button>
-              </Right>
-            </CardItem>
-          </Card>
-        }
-        />
-        </Content>
-        <View style={{ height: 40, padding: 10, flexDirection: 'row', width: 380, borderBottomWidth: 1, borderColor: '#a2aab0' }}>
-          <Text style={{ alignSelf: 'flex-start', fontWeight: 'bold', color: '#3e3e3b' }}>Total Price: </Text>
-        </View>
-        <View style={{ height: 65, padding: 10, flexDirection: 'row', width: 400, }}>
-          <Button block style={{ marginRight: 130, alignSelf: 'flex-start', borderRadius: 5 }}>
-            <Text style={{ fontWeight: 'bold' }}>FITTING ROOM</Text>
-          </Button>
-          <Button block style={{ borderRadius: 5 }}>
-            <Text style={{ fontWeight: 'bold', borderRadius: 5 }}>CHECKOUT</Text>
-          </Button>
-        </View>
-      </Container>
-    )
+    const { user, loading, error } = this.props.data
+    const { navigation } = this.props
+    console.log('props >>>>>', this.props)
+    console.log('TOTALPRICE > ', this.state.totalPrice)
+    if (loading) {
+      return <Text>Loading . . .</Text>
+    } else {
+      return (
+        <Container style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ebeced' }}>
+          <Content style={{ padding: 10, marginTop: 25, marginBottom: 10, width: 400 }}>
+            <FlatList
+            data={user.cart}
+            keyExtractor={item => item._id}
+            renderItem={({item}) => <ListCart forTotalPrice={this.forTotalPrice} item={ item } price={item.price} navigation={ navigation }/>}
+            />
+          </Content>
+          <Total data={user.cart}/>
+          <View style={{ height: 65, padding: 10, flexDirection: 'row', width: 400, }}>
+            <Button block style={{ marginRight: 130, alignSelf: 'flex-start', borderRadius: 5 }}>
+              <Text style={{ fontWeight: 'bold' }}>FITTING ROOM</Text>
+            </Button>
+            <Button block style={{ borderRadius: 5 }}>
+              <Text style={{ fontWeight: 'bold', borderRadius: 5 }}>CHECKOUT</Text>
+            </Button>
+          </View>
+        </Container>
+      )
+    }
   }
 }
 
+export default graphql(product.users)(Cart)
