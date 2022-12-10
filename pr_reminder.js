fa33ec1697fc31e36f8dd8506e7df47697f6f711
Renal Apriansyah <renal.apr@gmail.com>
2022-12-10 09:11:03 +0700

    fix reminder

--- pr_reminder.js
+++ pr_reminder.js
@@ -1,24 +1,16 @@
 const core = require("@actions/core");
-const fetch = require("node-fetch");
+const axios = require('axios');
 
 (async () => {
   try {
     
     const payload = {
-      text: `test 1 ${core.getInput('SLACK_WEBHOOK_URL')}`
+      text: `<@userID> test 1 ${core.getInput('SLACK_WEBHOOK_URL')}`
     }
 
-    const res = await fetch("https://hooks.slack.com/services/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", {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        Accept: "application/json",
-      },
-    });
-    if (!res.ok) {
-      throw new Error(`Server error ${res.status}`);
-    }
+    await axios.post(
+      "https://hooks.slack.com/services/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", 
+      payload);
   } catch (error) {
     core.setFailed(error.message);
   }
