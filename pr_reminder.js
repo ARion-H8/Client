0d9dbe7610fb63558ecc5b1b9e4f07540972e6f0
Renal Apriansyah <renal.apr@gmail.com>
2022-12-10 10:34:30 +0700

    map slack id

--- pr_reminder.js
+++ pr_reminder.js
@@ -3,16 +3,20 @@ const axios = require('axios');
 
 (async () => {
   try {
+  
     console.log(core.getInput('pr-reviewers'))
     
+    const reviewers = JSON.parse(core.getInput('pr-reviewers')).map(d => d.login)
+    
     const payload = {
-      text: `<@userID> test 1 ${JSON.stringify(core.getInput('pr-reviewers'))}`
+      text: `<@userID> test 1 ${reviewers.join(',')}`
     }
 
     await axios.post(
       `${core.getInput('slack-webhook-url')}`, 
       payload);
   } catch (error) {
+    console.log(error)
     core.setFailed(error.message);
   }
 })();
